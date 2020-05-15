cc9da3439c7915c386b2f143874e1ce16e957bf1
Mark <mgarnick@techtonicgroup.com>
2020-05-15 16:20:41 -0600

    setup bulma and app layout

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import AppLayout from './theme/Layout.vue';
+import './assets/main.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(AppLayout),
+}).$mount('#app');
---
+++ src/theme/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav class="navbar has-shadow">
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          <img alt="vue logo" :style="{ width: '80px', height: '80px' }" src="../assets/logo.png" />
+        </a>
+      </div>
+    </nav>
+    <section class="section">
+    </section>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          Follow us on
+          <a href="https://twitter.com/bstravroulakis" target="_blank">Twitter </a>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style style="scss">
+</style>
