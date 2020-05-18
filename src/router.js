28a5cff3d54464884336546970697ef9c38c74ec
Mark <mgarnick@techtonicgroup.com>
2020-05-18 11:14:33 -0600

    added commented lazy loading options

--- src/router.js
+++ src/router.js
@@ -4,6 +4,10 @@ import Category from './theme/Category.vue';
 import Login from './theme/Login.vue';
 import NotFound from './theme/NotFound.vue';
 
+// const Category = () => import('./theme/Category.vue');
+// const Login = () => import('./theme/Login.vue');
+// const NotFound =() => import('./theme/NotFound.vue');
+
 Vue.use(VueRouter);
 
 const routes = [
