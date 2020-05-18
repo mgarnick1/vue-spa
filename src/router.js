7288d972f37e7349622617a25d4174a919e12aa8
Mark <mgarnick@techtonicgroup.com>
2020-05-18 10:18:25 -0600

    added login path and component

--- src/router.js
+++ src/router.js
@@ -1,13 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Category from './theme/Category.vue';
+import Login from './theme/Login.vue';
 
 Vue.use(VueRouter);
 
-const routes = [{ path: '/', component: Category }]
+const routes = [
+  { path: '/', component: Category },
+  { path: '/login', component: Login },
+];
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  routes,
 });
 
 export default router;
