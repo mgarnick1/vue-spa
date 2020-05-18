6659d40f460e17a491c94eb6447c84d96f9b1fa6
Mark <mgarnick@techtonicgroup.com>
2020-05-18 15:26:34 -0600

    added eventbus

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -2,17 +2,35 @@
   <nav class="navbar has-shadow">
     <div class="container">
       <router-link class="navbar-brand" to="/" exact>
-        <img alt="Vue SPA logo" :style="{ width: '80px', height: '80px' }" src="../assets/logo.png" />
+        <img
+          alt="Vue SPA logo"
+          :style="{ width: '80px', height: '80px' }"
+          src="../assets/logo.png"
+        />
       </router-link>
       <router-link class="navbar-item is-tab" to="/category/front-end">Front-end</router-link>
-      <router-link class="navbar-item is-tab" :to="{ name: 'category', params: { id: 'mobile' } }">Mobile</router-link>
-      <router-link class="navbar-item is-tab" to="/login">Login</router-link>
+      <router-link class="navbar-item is-tab" :to="{ name: 'category', params: { id: 'mobile' } }"
+        >Mobile</router-link
+      >
+      <router-link class="navbar-item is-tab" to="/login"
+        ><span v-if="isAuthenticated">Logout</span><span v-else>Login</span></router-link
+      >
     </div>
   </nav>
 </template>
 
 <script>
+import eventBus from '../event-bus.js';
 export default {
-    
-}
+  data() {
+    return {
+      isAuthenticated: false,
+    };
+  },
+  created() {
+    eventBus.$on('authStatusUpdate', (isAuthenticated) => {
+      this.isAuthenticated = isAuthenticated;
+    });
+  },
+};
 </script>
--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -49,6 +49,7 @@
 </template>
 <script>
 import appService from '../app.service.js';
+import eventBus from '../event-bus.js';
 export default {
   data() {
     return {
@@ -67,6 +68,7 @@ export default {
       } else {
         this.profile = {};
       }
+      eventBus.$emit('authStatusUpdate', val);
     },
   },
   methods: {
