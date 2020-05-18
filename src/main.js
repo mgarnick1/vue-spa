a9b6b0d7c172635a244f9eadca27c5064aff79c1
Mark <mgarnick@techtonicgroup.com>
2020-05-18 15:32:15 -0600

    added vuex and initialized isAuthenticated

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import AppLayout from './theme/Layout.vue';
 import './assets/main.scss';
 import router from './router.js';
+import store from './vuex/index.js';
 
 Vue.config.productionTip = false;
 
 
 new Vue({
   router,
+  store,
   render: (h) => h(AppLayout),
 }).$mount('#app');
 
